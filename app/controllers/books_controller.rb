1358bd72c7b1865b4929f0171c049bf75ce736a9
EC2 Default User <ec2-user@ip-172-31-30-218.ec2.internal>
2021-11-02 07:44:59 +0000

    [Update] bookの新規登録とcreate機能、indexのページを作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,27 @@
 class BooksController < ApplicationController
   def index
+    # リスト一覧を取得する
+    @books = Book.all
+    # 新規投稿用のBook.newを取得する
+    @book = Book.new
   end
 
   def show
+
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def edit
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
