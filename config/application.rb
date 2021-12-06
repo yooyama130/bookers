4168184addcfd3d2380347c228dcb2f675f6b2d1
EC2 Default User <yooyama.97.01@gmail.com>
2021-12-06 07:50:51 +0000

    [Finish] 開発スキルアップ 【Rails】バッチ処理を学ぼう

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module Bookers
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.paths.add 'lib', eager_load: true
+    config.enable_dependency_loading = true
+    config.autoload_paths += Dir["#{config.root}/lib"]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
